d8aa33cd84aaac217a9b9e7b6c4a0c1fa0f02046
LarsVomMars <developer@kroenner.eu>
2024-12-27 17:21:54 +0100

    welp

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "L.A.R.S Blog";
+export const SITE_DESCRIPTION = "Welcome to our blog";
+export const SITE_BASE = "/astro-blog";
